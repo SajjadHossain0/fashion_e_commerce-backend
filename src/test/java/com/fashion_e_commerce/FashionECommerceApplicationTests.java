0262ee6a0f209eb469e84e28cd83541f9fcc3e18
mdsaj <mdsajjadhossain614@gmail.com>
2024-11-13 22:35:56 +0600

    new commit

---
+++ src/test/java/com/fashion_e_commerce/FashionECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashion_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
